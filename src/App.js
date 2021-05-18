99f467de0d0809d93a2af8b16752c40507a25014
Mihai Ivanov <mikee2988@yahoo.com>
2021-05-18 19:31:05 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Details from "components/Details";
-import MainPage from "components/MainPage";
+import Details from "./components/Details";
+import MainPage from "./components/MainPage";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
